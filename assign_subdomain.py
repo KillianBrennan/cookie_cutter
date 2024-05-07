2fdc26fcebb6f4e815a81cc752d28ded76d58c6e
Killian Brennan <kbrennan@env.ethz.ch>
2024-05-07 17:03:26 +0200

    working implementation of subdomain assignment and composite calculation

--- assign_subdomain.py
+++ assign_subdomain.py
@@ -22,25 +22,20 @@ Killian P. Brennan
 """
 
 import os
-import shutil
-
-import xarray as xr
-import numpy as np
-
 import argparse
+import xarray as xr
 
 
 def main(cookie_dir, subdomain_dir):
 
-    subdomains = xr.open_dataset(
-        "/home/kbrennan/phd/data/climate/grids/subdomains_rot.nc"
-    )
+    subdomains = xr.open_dataset(subdomain_dir)
     subdomains = subdomains.drop(["time_bnds", "rotated_pole"])
     subdomains = subdomains.squeeze()
 
+    os.makedirs(os.path.join(cookie_dir,'subdomains'), exist_ok=True)
     keys = list(subdomains.keys())
     for k in keys:
-        os.makedirs(os.path.join(cookie_dir, k), exist_ok=True)
+        os.makedirs(os.path.join(cookie_dir,'subdomains', k), exist_ok=True)
 
     cookie_files = os.listdir(cookie_dir)
 
@@ -55,19 +50,26 @@ def main(cookie_dir, subdomain_dir):
         rlon = cookie.rlon.sel(x=0, y=0).values
         rlat = cookie.rlat.sel(x=0, y=0).values
         for dom in subdomains.keys():
-            test = subdomains[dom].sel(
-                rlon=rlon,
-                rlat=rlat,
-                method="nearest",
-            ).values
+            test = (
+                subdomains[dom]
+                .sel(
+                    rlon=rlon,
+                    rlat=rlat,
+                    method="nearest",
+                )
+                .values
+            )
             if test == 1.0:
-                shutil.copy(os.path.join(cookie_dir, f), os.path.join(cookie_dir, dom, f))
                 print(f"Assigning {f} to {dom}")
+                os.symlink(
+                    os.path.join(cookie_dir, f),
+                    os.path.join(cookie_dir, 'subdomains', dom, f),
+                )
     return
+
 
 def determine_subdomain(cookie, subdomains):
     """figure out which subdomain each cookie belongs to"""
-    
 
 
 if __name__ == "__main__":
--- calculate_composites.py
+++ calculate_composites.py
@@ -7,22 +7,45 @@ import argparse
 
 
 def main(cookie_dir):
-    os.makedirs('composites', exist_ok=True)
+    os.makedirs("composites", exist_ok=True)
 
-    subdomains = xr.open_dataset('/home/kbrennan/phd/data/climate/grids/subdomains_rot.nc')
-    subdomains = subdomains.drop(['time_bnds','rotated_pole'])
-    subdomains = subdomains.squeeze()
+    subdomain_dir = os.path.join(cookie_dir, "subdomains")
 
-    cookies = load_cookies(cookie_dir)
-
+    for dom in os.listdir(subdomain_dir):
+        if os.path.isdir(os.path.join(subdomain_dir, dom)):
+            print(dom)
+            cookies = load_cookies(os.path.join(subdomain_dir, dom))
+            mean = calculate_composite(cookies)
+            mean.to_netcdf(os.path.join("composites", f"{dom}_mean.nc"))
     return
 
+
 def load_cookies(cookie_dir):
-    cookies = xr.open_mfdataset(os.path.join(cookie_dir, '*.nc'))
+    cookies = xr.open_mfdataset(
+        os.path.join(cookie_dir, "*.nc"), combine="by_coords", parallel=True
+    )
     return cookies
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Calculate composites from cookies')
-    parser.add_argument('cookie_dir', type=str, help='Directory containing cookies')
+
+def calculate_composite(cookies):
+    n_cookies = cookies.cookie_id.shape[0]
+    n_fields = cookies.T.count(dim='cookie_id')
+
+    mean = cookies.mean(dim="cookie_id", skipna=True)
+    mean = mean.compute()
+    mean['n_cookies'] = n_cookies
+    mean['n_cookies'].attrs = {
+        'long_name': 'number of cookies in composite'
+    }
+    mean['n_fields'] = n_fields 
+    mean['n_fields'].attrs = {
+        'long_name': 'number of fields in composite'
+    }
+    return mean
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Calculate composites from cookies")
+    parser.add_argument("cookie_dir", type=str, help="Directory containing cookies")
     args = parser.parse_args()
     main(args.cookie_dir)
